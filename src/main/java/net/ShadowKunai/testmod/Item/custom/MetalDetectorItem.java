0de220740590e323719f68ed85fcd34e9cfd66e2
BarmettlerL-bzz <BarmettlerL@bzz.ch>
2024-08-28 16:16:08 +0200

    textures item (ore) fixed and metal detector started

---
+++ src/main/java/net/ShadowKunai/testmod/Item/custom/MetalDetectorItem.java
@@ -0,0 +1,9 @@
+package net.ShadowKunai.testmod.Item.custom;
+
+import net.minecraft.world.item.Item;
+
+public class MetalDetectorItem extends Item {
+    public MetalDetectorItem(Properties pProperties){
+        super(pProperties);
+    }
+}
